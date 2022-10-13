33192153032259719400c5b443cc21da6341a4df
rannue <yavianbo@gmail.com>
2022-10-14 02:36:19 +0300

    add some pass tests of exended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = ''
+  let separatorForStr = (options.separator || '+')
+  let separatorForAdd = (options.additionSeparator || '|')
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result += str;
+
+
+    for (let k = 0; k < options.additionRepeatTimes; k++) {
+      result += options.addition + separatorForAdd;
+    }
+
+    if (options.addition) {
+      result = result.slice(0, -separatorForAdd.length)
+    }
+
+    result += separatorForStr;
+
+  }
+
+  result = result.slice(0, -separatorForStr.length)
+
+  console.log(result)
+  console.log(options)
+  console.log(options.repeatTimes)
+  return result
+
+
 }
+
+repeater('la', { repeatTimes: 3 })
 
 module.exports = {
   repeater
